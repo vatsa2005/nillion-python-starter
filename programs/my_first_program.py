7046ca3af52170bfe254d3bd57a829eea397ee8a
vatsa2005 <svatsa2005@gmail.com>
2024-06-21 19:18:39 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -8,5 +8,5 @@ def nada_main():
 
     # write the computation for your program here - use my_int1 and my_int2 as inputs
     # make sure you change the output below to be your new output
-
-    return [Output(my_int1, "my_output", party1)]
+    result = my_int1 % my_int2
+    return [Output(result, "my_output", party1)]
